8205d524a9609baf440262553d98ca7488ed3c19
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-21 11:39:00 +0900

    linking between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
-    return <div>home page</div>;
+    return (
+        <div>
+            Home page
+            <div className="flex flex-col p-5">
+                <Link href="/performance">performance</Link>
+                <Link href="/scale">scale</Link>
+                <Link href="/reliability">reliability</Link>
+            </div>
+        </div>
+    );
 }
