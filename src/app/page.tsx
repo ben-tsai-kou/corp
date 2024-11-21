1a11f9d94edb9c663abcdf373b74295617508795
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-21 11:45:49 +0900

    common ui in next js with layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Link from 'next/link';
-
 export default function Home() {
-    return (
-        <div>
-            Home page
-            <div className="flex flex-col p-5">
-                <Link href="/performance">performance</Link>
-                <Link href="/scale">scale</Link>
-                <Link href="/reliability">reliability</Link>
-            </div>
-        </div>
-    );
+    return <div>Home page</div>;
 }
